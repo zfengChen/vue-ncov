c7c1cf513593b0472073688bac1e9a524b3639ee
zfengChen <1029251038@qq.com>
2022-01-08 22:54:48 +0800

    master

--- vue.config.js
+++ vue.config.js
@@ -10,7 +10,7 @@ module.exports = {
       }
     }
   },
-  publicPath: process.env.NODE_ENV === 'production' ? '/vue-ncov' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/dist' : '/',
   configureWebpack: {
     performance: {
       hints: 'warning',
