7370fe5da2d0baffa0f73eb0de93a370fa06ddac
zfengChen <1029251038@qq.com>
2022-01-09 13:42:00 +0800

    master2

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,8 @@
 
     <router-view />
 
-    <van-tabbar v-model="active" placeholder>
-      <van-tabbar-item icon="home-o" to="/">首页</van-tabbar-item>
+    <van-tabbar route placeholder>
+      <van-tabbar-item icon="home-o" to="/home">首页</van-tabbar-item>
       <van-tabbar-item icon="guide-o" to="/map">疫情地图</van-tabbar-item>
       <van-tabbar-item icon="chat-o" to="/news">最新消息</van-tabbar-item>
     </van-tabbar>
@@ -16,7 +16,6 @@
 export default {
   data () {
     return {
-      active: 0,
       placeholder: true
     }
   }
